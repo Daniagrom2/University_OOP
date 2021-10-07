e6756a5cf1537e92d532c8619746e40a941f31d4
Daniagrom2 <daniagrom5@gmail.com>
2021-10-07 08:39:00 +0300

    oop lsb1

---
+++ OOP_1/cmake-build-debug/CMakeFiles/OOP_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_1.dir/main.cpp.o"
+  "OOP_1"
+  "OOP_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
